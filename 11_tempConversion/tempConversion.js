0e1af431bc04dafd6a361e848e1803da2e7e5333
Jaesuru <jasongsm2017@gmail.com>
2025-11-28 14:57:03 -0500

    complete 11_tempConversion assignment
    Implement two methods that properly convert
    temperature metrics and rounds it to the nearest
    tenth decimal place.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let newTemp = fah;
+
+  newTemp = (newTemp - 32) * (5 / 9);
+
+  return Number(newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let newTemp = cel;
+
+  newTemp = (newTemp * 1.8) + 32;
+  
+  return Number(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
